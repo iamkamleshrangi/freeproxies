9ced1e1c244b56eadcfc3e1a593ff93642c83acf
Kamlesh Kumar Rangi <iamkamleshrangi@gmail.com>
2019-05-08 11:45:23 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+import codecs
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def abs_path(*relative_path_parts):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                        *relative_path_parts)
+
+
+name = 'freeproxy'
+
+with codecs.open(abs_path(name, '__init__.py'), 'r', 'utf-8') as fp:
+    try:
+        version = re.findall(r"^__version__ = '([^']+)'.*?$",
+                             fp.read(), re.M)[0]
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
+
+setup(
+    name=name,
+    version=version,
+    url='https://github.com/iamkamleshrangi/freeproxy',
+    download_url='https://github.com/s0md3v/hue.git',
+    author='Kamlesh Kumar Rangi',
+    author_email='iamkamleshrangi@gmail.com',
+    description='Free Proxies for human',
+    keywords='free proxy, free proxies, freeproxy, freeproxies, proxies, proxy, github proxies',
+    packages=find_packages(),
+    py_modules=['freeproxy'],
+    data_files=[('', ['LICENSE'])],
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ])
