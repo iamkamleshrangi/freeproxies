10497cb6503515b164fc7a01294eea174e737a1e
Kamlesh Kumar Rangi <iamkamleshrangi@gmail.com>
2019-05-11 18:56:35 +0530

    Added setup file

--- setup.py
+++ setup.py
@@ -1,50 +1,21 @@
-import codecs
-import os
-import re
+import setuptools
 
-from setuptools import find_packages, setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-
-def abs_path(*relative_path_parts):
-    return os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                        *relative_path_parts)
-
-
-name = 'freeproxy'
-
-with codecs.open(abs_path(name, '__init__.py'), 'r', 'utf-8') as fp:
-    try:
-        version = re.findall(r"^__version__ = '([^']+)'.*?$",
-                             fp.read(), re.M)[0]
-    except IndexError:
-        raise RuntimeError('Unable to determine version.')
-
-setup(
-    name=name,
-    version=version,
-    url='https://github.com/iamkamleshrangi/freeproxy',
-    download_url='https://github.com/iamkamleshrangi/freeproxy.git',
-    author='Kamlesh Kumar Rangi',
-    author_email='iamkamleshrangi@gmail.com',
-    description='Free Proxies for human',
-    keywords='free proxy, free proxies, freeproxy, freeproxies, proxies, proxy, github proxies',
-    packages=find_packages(),
-    py_modules=['freeproxy'],
-    data_files=[('', ['LICENSE'])],
-    include_package_data=True,
+setuptools.setup(
+    name="freeproxies",
+    version="0.0.1",
+    author="Kamlesh Kumar Rangi",
+    author_email="iamkamleshrangi@gmail.com",
+    description="free proxies for humans",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iamkamleshrangi/freeproxies",
+    packages=setuptools.find_packages(),
     classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Intended Audience :: Developers',
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Operating System :: OS Independent',
-        'Environment :: Console',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ])
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
